1030ad765f9ed2755e794dbeb179edf2f296e096
a1876010 <a1876010@adelaide.edu.au>
2025-03-16 00:34:59 +1030

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -1,115 +1,90 @@
+import signal
 import socket
-import sys
-import os
-import argparse
-import re
-import hashlib
 import threading
 
-BUFFER_SIZE = 1000000  # 1MB buffer size
+class proxy():
+    def __init__(self):
+        # creating a tcp socket
+        self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Get the IP address and Port number to use for this web proxy server
-parser = argparse.ArgumentParser()
-parser.add_argument('hostname', help='The IP Address of Proxy Server')
-parser.add_argument('port', type=int, help='The port number of the proxy server')
-args = parser.parse_args()
-proxyHost = args.hostname
-proxyPort = args.port
+        # reuse the socket
+        self.serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-# Create a server socket
-try:
-    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serverSocket.bind((proxyHost, proxyPort))
-    serverSocket.listen(10)
-    print(f'Proxy server listening on {proxyHost}:{proxyPort}')
-except socket.error as err:
-    print(f'Failed to set up server: {err}')
-    sys.exit()
+        self.your_ip = "127.0.0.1" # loop back address
+        self.your_port = 80 # use a port thats open like port 80
 
-def handle_client(clientSocket):
-    try:
-        message_bytes = clientSocket.recv(BUFFER_SIZE)
-        message = message_bytes.decode('utf-8')
-        print('Received request:\n' + message)
-    except Exception as err:
-        print(f'Error receiving data: {err}')
-        clientSocket.close()
-        return
+    def shutdown(self):
+        # add your signal shutdown code here
+        pass
 
-    # Extract HTTP request details
-    requestParts = message.split('\r\n')[0].split()
-    if len(requestParts) < 3:
-        clientSocket.close()
-        return
-    method, URI, version = requestParts[:3]
-    print(f'Method: {method}\nURI: {URI}\nVersion: {version}')
+    def main(self):
+        # shutdown on cntrl c
+        signal.signal(signal.SIGINT, self.shutdown)
 
-    # Process the requested resource
-    URI = re.sub('^(/?)http(s?)://', '', URI, count=1).replace('/..', '')
-    resourceParts = URI.split('/', 1)
-    hostname = resourceParts[0]
-    resource = '/' + resourceParts[1] if len(resourceParts) == 2 else '/'
+        self.serverSocket.bind((self.your_ip, self.your_port))
 
-    # Hash the resource for caching
-    hashed_filename = hashlib.md5(resource.encode()).hexdigest()
-    cacheLocation = os.path.join('./cache', hostname, hashed_filename)
-    print(f'Cache location: {cacheLocation}')
+        self.serverSocket.listen(10)
+        self.__clients = {}
 
-    # Check if resource is in cache
-    if os.path.isfile(cacheLocation):
-        try:
-            with open(cacheLocation, 'rb') as cacheFile:
-                cacheData = cacheFile.read()
-            clientSocket.sendall(cacheData)
-            print(f'Cache hit! Sent cached response.')
-        except Exception as e:
-            print(f'Error reading cache: {e}')
-    else:
-        # Cache miss, connect to origin server
-        try:
-            originServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            address = socket.gethostbyname(hostname)
-            originServerSocket.connect((address, 80))
-            print(f'Connected to origin server: {hostname}')
+        while True:
+            # establish the connection
+            (clientSocket, client_address) = self.serverSocket.accept()
 
-            # Construct request to origin server
-            request = f"{method} {resource} {version}\r\nHost: {hostname}\r\nConnection: close\r\n\r\n"
-            originServerSocket.sendall(request.encode())
-            print('Forwarded request to origin server')
+            d = threading.Thread(name=self._getClientName(client_address),
+                                 target=self.proxy_thread,
+                                 args=(clientSocket, client_address))
+            d.setDaemon(True)
+            d.start()
 
-            # Receive response from origin server
-            response = b""
-            while True:
-                chunk = originServerSocket.recv(BUFFER_SIZE)
-                if not chunk:
-                    break
-                response += chunk
-            
-            # Send response to client
-            clientSocket.sendall(response)
-            print('Sent response to client')
+        # get the request from browser
+        request = conn.recv(4096)
 
-            # Cache the response
-            os.makedirs(os.path.dirname(cacheLocation), exist_ok=True)
-            with open(cacheLocation, 'wb') as cacheFile:
-                cacheFile.write(response)
-            print('Cached response')
-        except socket.error as err:
-            print(f'Error fetching from origin server: {err}')
-        finally:
-            originServerSocket.close()
-    
-    clientSocket.close()
+        # parse the first line
+        first_line = request.split('\n')[0]
 
-# Accept multiple connections using threading
-while True:
-    try:
-        clientSocket, clientAddress = serverSocket.accept()
-        print(f'Received a connection from {clientAddress}')
-        threading.Thread(target=handle_client, args=(clientSocket,)).start()
-    except KeyboardInterrupt:
-        print('\nShutting down proxy server.')
-        serverSocket.close()
-        sys.exit()
-    except socket.error as err:
-        print(f'Connection error: {err}')
+        # get url
+        url = first_line.split(' ')[1]
+
+        http_pos = url.find("://")
+        if http_pos == -1:
+            temp = url
+        else:
+            temp = url[(http_pos + 3):]
+
+        port_pos = temp.find(":")
+
+        # find end of web server
+        webserver_pos = temp.find("/")
+        if webserver_pos == -1:
+            webserver_pos = len(temp)
+
+        webserver = ""
+        port = -1
+
+        if port_pos == -1 or webserver_pos < port_pos:
+
+            # default port
+            port = 80
+            webserver = temp[:webserver_pos]
+
+        else:  # specific port
+            port = int((temp[(port_pos + 1):])[:webserver_pos - port_pos - 1])
+            webserver = temp[:port_pos]
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(1)
+        s.connect((webserver, port))
+        s.sendall(request)
+
+        while 1:
+            # receive data from web server
+            data = s.recv(4096)
+
+            if len(data) > 0:
+                conn.send(data)  # send to browser/client
+
+            else:
+                break
+
+p = proxy()
+p.main()
